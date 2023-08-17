0145b47ebc790ab980a8aa6390e7b4f12610cce6
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-17 13:23:40 -0500

    create component imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+// imports for components
+import {Header} from '..Header/Header';
+import {PizzaList} from '..PizzaList/PizzaList';
+import {CustomerForm} from '..CustomerForm/CustomerForm';
+import {Checkout} from '..Checkout/Checkout';
+import {PizzaAdmin} from '..PizzaAdmin/PizzaAdmin';
+
 
 function App() {
 
@@ -9,6 +16,11 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
+      <Header />
+      <PizzaList />
+      <CustomerForm />
+      <Checkout/>
+      <PizzaAdmin />
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
