fda004ee23395e8beaf4a40fe5aca8912eef5b9a
Gabe Glasco <gabeglasco@gmail.com>
2023-08-17 13:39:33 -0500

    add routes and links

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import PizzaList from '../PizzaList/PizzaList'
 
 function App() {
 
@@ -9,9 +11,31 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
+    <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+
+      <Router>
+        <div>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+             <li>
+              <Link to="/order">Order Pizza</Link>
+            </li>
+          </ul>
+        <Route exact path="/">
+          {/* <Home /> */}
+        </Route>
+        <Route exact path="/order">
+          <PizzaList />
+        </Route>
+        <Route exact path="/form">
+          <CustomerForm />
+        </Route>
+      
+        </div>
+      </Router>
   
     </div>
   );
