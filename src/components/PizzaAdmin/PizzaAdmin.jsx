740a5f313f4c7163b0c68ce56e9eaf90638ec183
noelwschneider <noelwschneider@gmail.com>
2023-08-17 14:10:07 -0500

    wrote dummy text for Header.jsx and PizzaAdmin.jsx

--- src/components/PizzaAdmin/PizzaAdmin.jsx
+++ src/components/PizzaAdmin/PizzaAdmin.jsx
@@ -1,7 +1,40 @@
 function PizzaAdmin() {
 
-    return(<>
-        <p>PizzaAdmin.jsx</p>
+    return (<>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Time Order Placed</th>
+                    <th>Type</th>
+                    <th>Cost</th>
+                </tr>
+            </thead>
+
+            {/* Table rows will be written in a component called PizzaAdminItem.jsx or something. We can use .map to get an item component for each previous order. -noel */}
+            {/* Data for order history will likely come from Database -noel */}
+            <tr>
+                <td>Example name</td>
+                <td>Example time</td>
+                <td>Example type</td>
+                <td>Example cost</td>
+            </tr>
+
+            <tr>
+                <td>Example name</td>
+                <td>Example time</td>
+                <td>Example type</td>
+                <td>Example cost</td>
+            </tr>
+
+            <tr>
+                <td>Example name</td>
+                <td>Example time</td>
+                <td>Example type</td>
+                <td>Example cost</td>
+            </tr>
+           
+        </table>
     </>)
 }
 
