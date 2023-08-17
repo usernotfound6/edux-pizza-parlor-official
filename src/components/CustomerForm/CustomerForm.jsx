4800e5dfe76eb66adbc33bb7189fd5488d50be57
fraol <phrolaye@gmail.com>
2023-08-17 15:27:28 -0500

    started form

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,8 +1,50 @@
 function CustomerForm() {
 
-    return(<>
-        <p>CustomerForm.jsx</p>
-    </>)
+    return (
+      <form>
+        <h1>Step 2: Customer Information</h1>
+        <input id="Name"
+               type="text" 
+               placeholder="Name">
+        </input>
+          <div>
+            <input id="Address"
+                   type="text"
+                   placeholder="Street Address">
+            </input>
+         </div>
+        <div>
+             <input id="City" 
+                    type="text" 
+                    placeholder="City">
+            </input>
+        </div>
+        <div>
+             <input id="Zip" 
+                    type="text" 
+                    placeholder="Zip">
+            </input>
+        </div>
+    <div>
+
+  <input type="radio" 
+         id="Pickup" 
+         name="Preferance" 
+         value="Pickup"></input>
+  <label for="Pickup">Pickup</label>
+  </div>
+  <div>
+  <input type="radio" 
+         id="Delivery" 
+         name="Preferance" 
+         value="Delivery"></input>
+  <label for="Delivery">Delivery</label>
+  
+  </div>
+      </form>
+
+      
+    );
 }
 
 export default CustomerForm
