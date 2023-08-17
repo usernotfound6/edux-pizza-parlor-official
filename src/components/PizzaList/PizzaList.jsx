3a99c200b325ec82cfe6d78c0d52e029ed74c3d7
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-17 15:10:09 -0500

    base display functionality for data

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,8 +1,20 @@
-function PizzaList() {
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { PizzaItem } from './Pizza Item/PizzaItem';
 
+function PizzaList() {
+    
     return(<>
-        <p>PizzaList.jsx</p>
-    </>)
+    {/* Header */}
+    <p>Step 1: Select Your Pizza</p>
+        <br></br>
+        <PizzaItem />
+        <br></br>
+        <br></br>
+        <Link to="/form">
+            <button>Next</button>
+        </Link>
+    </>
+    )
 }
 
 export default PizzaList
