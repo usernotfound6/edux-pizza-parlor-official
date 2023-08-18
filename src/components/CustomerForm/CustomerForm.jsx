13db6129e1b5b4e22e671e3665fb572b3e988027
fraol <phrolaye@gmail.com>
2023-08-18 12:00:14 -0500

    form done and reducer made

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,59 +1,113 @@
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 function CustomerForm() {
+       
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [zip, setZip] = useState("");
 
+  const dispatch = useDispatch();
 
+  const enterName = (event) => {
+    setName(event.target.value);
+  };
 
-        <Link to="/checkout">
-            <button>Next</button>
-        </Link>
-    </>)
-    return (
-      <form>
-        <h1>Step 2: Customer Information</h1>
-        <input id="Name"
-               type="text" 
-               placeholder="Name">
-        </input>
-          <div>
-            <input id="Address"
-                   type="text"
-                   placeholder="Street Address">
-            </input>
-         </div>
-        <div>
-             <input id="City" 
-                    type="text" 
-                    placeholder="City">
-            </input>
-        </div>
-        <div>
-             <input id="Zip" 
-                    type="text" 
-                    placeholder="Zip">
-            </input>
-        </div>
-    <div>
+  const enterAddress = (event) => {
+    setAddress(event.target.value);
+  };
 
-  <input type="radio" 
-         id="Pickup" 
-         name="Preferance" 
-         value="Pickup"></input>
-  <label for="Pickup">Pickup</label>
-  </div>
-  <div>
-  <input type="radio" 
-         id="Delivery" 
-         name="Preferance" 
-         value="Delivery"></input>
-  <label for="Delivery">Delivery</label>
-  
-  </div>
-      </form>
+  const enterCity = (event) => {
+    setCity(event.target.value);
+  };
 
-      
-    );
+  const enterZip = (event) => {
+    setZip(event.target.value);
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name === "" || address === "" || city === "" || zip === "") {
+      alert("Please fill out all info");
+    } else {
+      const data = {
+        name: name,
+        address: address,
+        city: city,
+        zip: zip,
+      };
+      // dispatch(addInfo(data));
+      dispatch({
+        type: "SET_CUST_INFO",
+        payload: data,
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Step 2: Customer Information</h1>
+      <input
+        id="Name"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={enterName}
+      ></input>
+      <div>
+        <input
+          id="Address"
+          type="text"
+          placeholder="Street Address"
+          value={address}
+          onChange={enterAddress}
+        ></input>
+      </div>
+      <div>
+        <input
+          id="City"
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={enterCity}
+        ></input>
+      </div>
+      <div>
+        <input
+          id="Zip"
+          type="text"
+          placeholder="Zip"
+          value={zip}
+          onChange={enterZip}
+        ></input>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="Pickup"
+          name="Preferance"
+          value="Pickup"
+        ></input>
+        <label>Pickup</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="Delivery"
+          name="Preferance"
+          value="Delivery"
+        ></input>
+        <label>Delivery</label>
+      </div>
+      <Link to="/checkout">
+        <button type="submit" onClick={handleSubmit}>
+          Next
+        </button>
+      </Link>
+    </form>
+  );
 }
 
-export default CustomerForm
+export default CustomerForm;
